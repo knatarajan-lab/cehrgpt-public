709be634c28a107ed668334c8f95c0884c4a24eb
Chao Pang <ChaoPang229@gmail.com>
2024-09-16 11:10:19 -0400

    convert input_ids back to concept_ids so concept_ids can be used for identifying the visit start and generating labels for time to event

--- src/cehrgpt/data/hf_cehrgpt_dataset_collator.py
+++ src/cehrgpt/data/hf_cehrgpt_dataset_collator.py
@@ -238,7 +238,10 @@ class CehrGptDataCollator:
         Adding the start and end indices to extract a portion of the patient sequence
         """
         # concept_ids will be used to for time to event predictions and identifying the visit starts
-        concept_ids = [self.tokenizer.decode(_) for _ in record['input_ids']]
+        input_ids = record['input_ids']
+        if isinstance(input_ids, torch.Tensor):
+            input_ids = input_ids.detach().tolist()
+        concept_ids = self.tokenizer.decode(input_ids)
         seq_length = len(record['input_ids'])
         new_max_length = self.max_length - 1  # Subtract one for the [END] token
 
